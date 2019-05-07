d5177d078a830b5443cecbcda488f4d256be9782
Brendan Byrd <GitHub@ResonatorSoft.org>
2019-05-06 20:12:41 -0400

    Initial commit

---
+++ Source/FactionBlender.cs
@@ -0,0 +1,206 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HugsLib;
+using Verse;
+
+namespace FactionBlender {
+    public class Base : ModBase {
+        public override string ModIdentifier {
+            get { return "FactionBlender"; }
+        }
+        public static Base Instance { get; private set; }
+        public Base() {
+            Instance = this;
+        }
+        public override void DefsLoaded() {
+            var FB_Factions = new List<FactionDef>();
+            FB_Factions.Add( FactionDef.Named("FactionBlender_Pirate") );
+            FB_Factions.Add( FactionDef.Named("FactionBlender_Civil")  );
+
+            Logger.Message("Scanning and inserting hair, backstory, trader kinds, and pawn groups");
+            foreach (FactionDef FBfac in FB_Factions) {
+                // NOTE: RemoveDuplicates only does an object comparison, which isn't good enough for these strings.
+            
+                // Add all hairTags
+                foreach (HairDef hair in DefDatabase<HairDef>.AllDefs) {
+                    foreach (var tag in hair.hairTags) {
+                        if (!FBfac.hairTags.Contains(tag)) FBfac.hairTags.Add(tag);
+                    }
+                }
+                FBfac.hairTags.RemoveDuplicates();
+
+                // Add all backstoryCategories
+                var bscat = FBfac.backstoryCategories;
+                foreach (var backstory in BackstoryDatabase.allBackstories) {
+                    foreach (var category in backstory.Value.spawnCategories) {
+                        if (!bscat.Contains(category)) bscat.Add(category);
+                    }
+                }
+                bscat.RemoveDuplicates();
+            }
+
+            // Fix caravanTraderKinds, visitorTraderKinds, baseTraderKinds for the civil faction only
+            FactionDef FB_Civil = FB_Factions[1];
+            foreach (var faction in DefDatabase<FactionDef>.AllDefs) {
+                if (faction == FB_Civil) continue;
+
+                foreach (var caravan in faction.caravanTraderKinds) {
+                    // Tribal caravans are just lesser versions of the outlander ones, except for the slavers, which we
+                    // don't want here.
+                    if (caravan.defName.Contains("Neolithic")) continue;
+                    FB_Civil.caravanTraderKinds.Add(caravan);
+                }
+                FB_Civil.visitorTraderKinds.AddRange(faction.visitorTraderKinds);
+                FB_Civil.   baseTraderKinds.AddRange(faction.   baseTraderKinds);
+            }
+            FB_Civil.caravanTraderKinds.RemoveDuplicates();
+            FB_Civil.visitorTraderKinds.RemoveDuplicates();
+            FB_Civil.   baseTraderKinds.RemoveDuplicates();
+
+            // Loop through each PawnKindDef
+            foreach (PawnKindDef pawn in DefDatabase<PawnKindDef>.AllDefs) {
+                RaceProperties race = pawn.RaceProps;
+
+                // Define weapon-like traits
+                bool isRanged = race.ToolUser && pawn.weaponTags != null && pawn.weaponTags.Any(t =>
+                    !(t.Contains("Melee") || t == "None") &&
+                    Regex.IsMatch(t, "Gun|Ranged|Pistol|Rifle|Sniper|Carbine|Revolver|Bowman|Grenade|Artillery|Assault|MageAttack|DefensePylon|GlitterTech|^OC|Federator|Ogrenaut|Hellmaker")
+                );
+                // Animals can shoot projectiles, too
+                if (race.Animal && pawn.race.Verbs != null && pawn.race.Verbs.Any(v =>
+                    v.burstShotCount >= 1 && v.range >= 10 && v.commonality >= 0.7 && v.defaultProjectile != null
+                )) isRanged = true;
+
+                bool isSniper = false;
+                if (isRanged) {
+                    // Using All here to be more strict about sniper weapon usage
+                    isSniper = race.ToolUser && pawn.weaponTags != null && pawn.weaponTags.All(t =>
+                        !(t.Contains("Melee") || t == "None") &&
+                        Regex.IsMatch(t, "Sniper|Ranged(Strong|Mighty|Heavy|Chief)|ElderThingGun")
+                    );
+                    if (race.Animal && pawn.race.Verbs != null && pawn.race.Verbs.Any(v =>
+                        v.burstShotCount >= 1 && v.range >= 40 && v.commonality >= 0.7 && v.defaultProjectile != null
+                    )) isSniper = true;
+                }
+
+                bool isHeavyWeapons = race.ToolUser && pawn.weaponTags != null && pawn.weaponTags.Any(t =>
+                    Regex.IsMatch(t, "Grenade|Flame|Demolition|GunHeavy|Turret|Pylon|Artillery|GlitterTech|OC(Heavy|Tank)|Bomb|Sentinel|FedHeavy")
+                );
+                // Include animals with BFGs and death explodey types
+                if (race.Animal) {
+                    if (isRanged && pawn.combatPower >= 500) isHeavyWeapons = true;
+                    if (
+                        race.deathActionWorkerClass != null &&
+                        Regex.IsMatch(race.deathActionWorkerClass.Name, "E?xplosion|Bomb")
+                    ) isHeavyWeapons = true;
+                }
+
+                /*
+                 * DEBUG
+                 *
+                string msg = pawn.defName;
+                msg += " (" + pawn.combatPower + "/" + race.baseBodySize + ") --> ";
+                if (race.Animal)     msg += "Animal, ";
+                if (race.ToolUser)   msg += "ToolUser, ";
+                if (race.Humanlike)  msg += "Humanlike, ";
+                if (pawn.isFighter)  msg += "Fighter, ";
+                if (pawn.trader)     msg += "Trader, ";
+                if (race.packAnimal) msg += "PackAnimal, ";
+                if (race.predator)   msg += "Predator, ";
+                if (isRanged)        msg += "Ranged, ";
+                if (!isRanged)       msg += "Melee, ";
+                if (isSniper)        msg += "Sniper, ";
+                if (isHeavyWeapons)  msg += "Heavy Weapons, ";
+
+                msg += "Speed: " + pawn.race.GetStatValueAbstract(StatDefOf.MoveSpeed);
+                if (isSniper || isHeavyWeapons) Logger.Message(msg);
+                */
+
+                if (pawn.defaultFactionType != null) {
+                    // No Vampires: Too many post-XML modifications and they tend to burn up on entry, anyway
+                    if (pawn.defaultFactionType.defName == "ROMV_Sabbat") continue;
+
+                    // No Star Vampires: They are loners that attack ANYBODY on contact, including their own faction
+                    if (pawn.defaultFactionType.defName == "ROM_StarVampire") continue;
+                }
+
+                foreach (FactionDef FBfac in FB_Factions) {
+                    foreach (PawnGroupMaker maker in FBfac.pawnGroupMakers) {
+                        bool isPirate = FBfac.defName == "FactionBlender_Pirate";
+                        bool isCombat = isPirate || (maker.kindDef.defName == "Combat");
+                        bool isTrader = maker.kindDef.defName == "Trader";
+
+                        // Allow "combat ready" animals
+                        int minCombatPower =
+                            isPirate ? 150 :
+                            isCombat ? 100 :
+                            50
+                        ;
+
+                        // Create the pawn option
+                        var newOpt = new PawnGenOption();
+                        newOpt.kind = pawn;
+                        newOpt.selectionWeight =
+                            race.Animal ? 1 :
+                            race.Humanlike ? 10 : 2
+                        ;
+
+                        if (isCombat) {
+                            // Gotta fight if you're in a combat raid
+                            if (!pawn.isFighter) continue;
+
+                            // If it's an animal, make sure Vegeta agrees with the power level
+                            if (race.Animal     && pawn.combatPower < minCombatPower)      continue;
+                            if (race.herdAnimal && pawn.combatPower < minCombatPower + 50) continue;
+
+                            // XXX: Unfortunately, there are no names for these pawnGroupMakers, so we have to use commonality
+                            // to identify each type.
+                            
+                            // Additional filters for specialized categories
+                            bool addIt = true;
+                            if      (maker.commonality == 65) addIt = isRanged;
+                            else if (maker.commonality == 60) addIt = !isRanged;
+                            else if (maker.commonality == 40) addIt = isSniper;
+                            else if (maker.commonality == 25) addIt = isHeavyWeapons;
+                            else if (maker.commonality == 10) newOpt.selectionWeight = race.Humanlike ? 1 : 10;
+
+                            // Add it
+                            if (addIt) maker.options.Add(newOpt);
+                        }
+                        else if (isTrader) {
+                            // Enforce a minimum speed.  Trader pawns shouldn't get left too far behind, especially pack animals.
+                            if (pawn.race.GetStatValueAbstract(StatDefOf.MoveSpeed) < 3) continue;
+                            
+                            // Trader group makers split up their pawns into three buckets.  The pawn will go into one of those
+                            // three, or none of them.
+                            if (pawn.trader) {
+                                maker.traders.Add(newOpt);
+                            }
+                            else if (
+                                race.packAnimal &&
+                                /* As amusing as it is watching chickenffalos hauling a bunch of goods with their tiny little bags,
+                                 * the game tends to unrealistically overload them with 20-25x their carry weight, instead of
+                                 * spawning more of them to carry the load.  And who the hell would actually use these to haul
+                                 * things, anyway?  No catfellos, either.
+                                 */
+                                race.baseBodySize >= 1
+                            ) {
+                                maker.carriers.Add(newOpt);
+                            }
+                            else if ((pawn.isFighter || race.predator) && pawn.combatPower >= minCombatPower) {
+                                maker.guards.Add(newOpt);
+                            }
+                        }
+                        else {
+                            // Peaceful or Settlement: Accept almost anybody
+                            maker.options.Add(newOpt);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/HarmonyPatches.cs
@@ -0,0 +1,166 @@
+﻿using Harmony;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+
+namespace FactionBlender {
+    [StaticConstructorOnStartup]
+    internal class HarmonyPatches {
+        /* Fix all of the biome.allowedPackAnimals, so that more animals can be proper pack animals.
+         *
+         * Honestly, putting animal-related information in BiomeDef is a bad idea, because NOBODY bothers
+         * to add anything in this field when creating a new animal.  I have RimWorld stuffed with just
+         * about every major animal mod out there, and without this fix, the only pack animals that manage
+         * to show up are Lórien deer and Muffalo.  No dinos, no genetic hybrids, no alpha animals, nothing
+         * except those two.
+         * 
+         * This _should_ be its own mod.  I might split this off eventually.
+         */
+
+        [HarmonyPatch(typeof(RimWorld.BiomeDef), "IsPackAnimalAllowed")]
+        public static class BiomeDef_IsPackAnimalAllowed_Patch {
+            /* XXX: Trying to figure out the min-max temperatures of a biome by indirect inference of the
+             * BiomeWorker scores is a very dicey at best.  I'm just going to resort to a static list, and
+             * it's not going to be perfect...
+             */
+
+            // Currently supports vanilla, Advanced Biomes, and Mallorn Forest (from Lord of the Rims - Elves)
+            public static readonly Dictionary<string, int> minBiomeTemp = new Dictionary<string, int> {
+                { "SeaIce",           -50 },
+                { "IceSheet",         -50 },
+                { "Tundra",           -50 },
+
+                { "BorealForest",     -35 },
+                { "ColdBog",          -35 },
+
+                { "TemperateForest",    -25 },
+                { "TemperateSwamp",     -25 },
+                { "MallornForest",      -25 },
+                { "TropicalRainforest", -25 },
+                { "PoisonForest",       -25 },
+                { "TropicalSwamp",      -25 },
+                { "Desert",             -25 },
+                { "Wasteland",          -25 },
+
+                { "AridShrubland",   0 },
+                { "ExtremeDesert",   0 },
+                { "Volcano",         0 },
+                { "Wetland",         0 },
+                { "Savanna",         0 },
+            };
+
+            public static readonly Dictionary<string, int> maxBiomeTemp = new Dictionary<string, int> {
+                { "SeaIce",   5 },
+                { "IceSheet", 5 },
+
+                { "Tundra",       25 },
+                { "BorealForest", 25 },
+                { "ColdBog",      25 },
+
+                { "TemperateForest",    35 },
+                { "TemperateSwamp",     35 },
+                { "MallornForest",      35 },
+                { "TropicalRainforest", 35 },
+                { "PoisonForest",       35 },
+                { "TropicalSwamp",      35 },
+                { "Desert",             35 },
+                { "Wasteland",          35 },
+                { "AridShrubland",      35 },
+                { "ExtremeDesert",      35 },
+                { "Volcano",            35 },
+                { "Wetland",            35 },
+                { "Savanna",            35 },
+            };
+
+            [HarmonyPostfix]
+            static void IsPackAnimalAllowed_Postfix(RimWorld.BiomeDef __instance, ref bool __result, List<ThingDef> ___allowedPackAnimals, ThingDef pawn) {
+                // If the original already gave us a positive result, just accept it and short-circuit
+                if (__result) return;
+
+                RaceProperties race = pawn.race;
+
+                // Needs to be a pack animal, first of all
+                if (!race.packAnimal) return;
+
+                // NOTE: For purposes of caching, we add the animal to ___allowedPackAnimals.  But,
+                // this doesn't cover negative caching.
+
+                // If it's already on the wildAnimal list, just accept it
+                if ( __instance.AllWildAnimals.Any(e => (e.defName == pawn.defName)) ) {
+                    __result = true;
+                    ___allowedPackAnimals.AddDistinct(pawn);
+                    return;
+                }
+
+                // Make sure the animal's comfort zone fits inside the min/max biome temperature ranges
+
+                string biomeName = __instance.defName;
+                if (!minBiomeTemp.ContainsKey(biomeName)) {
+                    // We don't have a temperature defined, so hope for the best
+                    Log.ErrorOnce(
+                        "[FactionBlender] Unrecognized biome " + biomeName + ".  Accepting all pack animals for traders here, which might cause some " +
+                        "to freeze/burn to death, if the biome is hostile.  Ask the dev to include the biome in the static min/max temp list.",
+                        __instance.debugRandomId + 1688085595
+                    );
+
+                    __result = true;
+                    ___allowedPackAnimals.AddDistinct(pawn);
+                    return;
+                }
+                else if (!maxBiomeTemp.ContainsKey(biomeName)) {
+                    Log.ErrorOnce(
+                        "[FactionBlender] Found minBiomeTemp without matching maxBiomeTemp for " + biomeName + "!  Dev goofed up!",
+                        __instance.debugRandomId + 1688085595
+                    );
+                    Log.TryOpenLogWindow();
+
+                    __result = true;
+                    ___allowedPackAnimals.AddDistinct(pawn);
+                    return;
+                }
+
+                int minTemp = minBiomeTemp[biomeName];
+                int maxTemp = maxBiomeTemp[biomeName];
+
+                if (
+                    pawn.GetStatValueAbstract(StatDefOf.ComfyTemperatureMin, null) <= minTemp &&
+                    pawn.GetStatValueAbstract(StatDefOf.ComfyTemperatureMax, null) >= maxTemp
+                ) {
+                    // Success
+                    __result = true;
+                    ___allowedPackAnimals.AddDistinct(pawn);
+                    return;
+                }
+
+                // Fell through: keep the false value
+                return;
+            }
+        }
+
+        /* Fix CanBeBuilder checks to not crash.
+         * 
+         * The core CanBeBuilder (and Torann's A RimWorld of Magic) makes some bad assumptions about what
+         * properties are available for pawns.  Some animals and non-humans might not have a story or
+         * definition.  So, protect all of that by checking all levels of expected objects for undefinedness.
+         */
+
+        [HarmonyPatch(typeof (LordToil_Siege), "CanBeBuilder", null)]
+        public static class CanBeBuilder_Patch {
+            [HarmonyPrefix]
+            private static bool Prefix(Pawn p, ref bool __result) {
+                if (
+                    // Carefully short-circuit, so that we don't cause our own ticking exception
+                    p == null ||
+                    p.def == null || p.story == null ||
+                    p.def.thingClass == null
+                ) {
+                    __result = false;
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
